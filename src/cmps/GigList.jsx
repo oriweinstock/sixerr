eb29516caf7e9d0656d53e966da6b1fb913bb6e3
maruanitomer <maruanitomer9@gmail.com>
2021-01-14 20:11:26 +0200

    details choose img feature

--- src/cmps/GigList.jsx
+++ src/cmps/GigList.jsx
@@ -3,11 +3,8 @@ import { GigPreview } from "./GigPreview"
 export function GigList({ gigs }) {
 
     return (
-        <section className="gig-list">
-            <h1>Recently added &amp; more</h1>
-            <ul className="clean-list">
+            <ul className="gig-list clean-list flex">
                 {gigs.map(gig => <GigPreview key={gig._id} gig={gig}></GigPreview>)}
             </ul>
-        </section>
     )
 }
