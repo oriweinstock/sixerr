3efa570444ef19add157995403a75228b561a51d
Ori Weinstock <ori.weinstock@gmail.com>
2021-01-14 23:11:31 +0200

    re-flexing gig list...

--- src/cmps/GigList.jsx
+++ src/cmps/GigList.jsx
@@ -3,8 +3,10 @@ import { GigPreview } from "./GigPreview"
 export function GigList({ gigs }) {
 
     return (
-            <ul className="gig-list clean-list flex">
+        <section className="gig-list">
+            <ul className="clean-list flex">
                 {gigs.map(gig => <GigPreview key={gig._id} gig={gig}></GigPreview>)}
             </ul>
+        </section>
     )
 }
